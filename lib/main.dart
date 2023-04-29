257569117ef784309c15aa0145cc24c51d9c6a1d
Muhammet Fatih Demir <130034989+mfatihdmr@users.noreply.github.com>
2023-04-29 08:47:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Temel Mesajlaşma Uygulaması",
+    return MaterialApp(
+      title: "Mesajlaşma Uygulaması Arayüzü",
       home: Iskele(),
     );
   }
 }
 
 class Iskele extends StatelessWidget {
-  const Iskele({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mesajlaşma Uygulaması Arayüzü"),
+        title: Text("Chat Uygulaması"),
+        backgroundColor: Color.fromARGB(255, 81, 217, 241),
       ),
       body: AnaEkran(),
     );
@@ -33,41 +32,76 @@ class AnaEkran extends StatefulWidget {
 
 class _AnaEkranState extends State<AnaEkran> {
   TextEditingController t1 = TextEditingController();
+  List<MesajBalonu> mesajListesi = [];
 
-  List mesajListesi = [];
-
-  mesajlariEkle(String metin) {
+  listeyeEkle(String gelenMesaj) {
     setState(() {
-      mesajListesi.insert(0, metin);
+      MesajBalonu mesajNesnesi = MesajBalonu(mesaj: gelenMesaj);
+      mesajListesi.insert(0, mesajNesnesi);
       t1.clear();
     });
+  }
+
+  Widget metinGirisAlani() {
+    return Container(
+      margin: EdgeInsets.all(15),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: t1,
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                listeyeEkle(t1.text);
+              },
+              icon: Icon(
+                Icons.send,
+              ))
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(40),
-      child: Column(
+      child: Column(children: [
+        Flexible(
+          child: ListView.builder(
+              itemCount: mesajListesi.length,
+              itemBuilder: (_, indeksNumarasi) => mesajListesi[indeksNumarasi]),
+        ),
+        Divider(
+          thickness: 1,
+        ),
+        metinGirisAlani(),
+      ]),
+    );
+  }
+}
+
+String isim = "Kullanıcı";
+
+class MesajBalonu extends StatelessWidget {
+  String mesaj;
+
+  MesajBalonu({required this.mesaj});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
         children: [
-          Flexible(
-              child: ListView.builder(
-            reverse: true,
-            itemCount: mesajListesi.length,
-            itemBuilder: (context, indeksNumarasi) =>
-                Text(mesajListesi[indeksNumarasi]),
-          )),
-          Row(
+          CircleAvatar(
+            child: Text(isim[0]),
+          ),
+          Column(
             children: [
-              Flexible(
-                child: TextField(
-                  onSubmitted: mesajlariEkle,
-                  controller: t1,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: mesajlariEkle(t1.text),
-                child: Text("Gönder"),
-              ),
+              Text(isim),
+              Text(mesaj),
             ],
           )
         ],
